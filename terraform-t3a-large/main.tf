88325cd6ad35e840dfca24e47dcc5109c1bfb86b
orvencasido <orvencasidop@gmail.com>
2026-04-28 13:52:40 +0800

    added bigger ec2 instance for testing

---
+++ terraform-t3a-large/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+# Security Group (ALLOW SSH + PORT 3000)
+resource "aws_security_group" "ssh" {
+  name        = "allow-ssh"
+  description = "Allow SSH access and app port 3000"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# SPOT EC2 INSTANCE UBUNTU
+resource "aws_instance" "test" {
+  ami           = "ami-02dd44faa40720bb8"
+  instance_type = "t3.large"
+
+  key_name = "n8n"
+
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      instance_interruption_behavior = "terminate"
+    }
+  }
+
+  tags = {
+    Name = "terraform-spot-test"
+  }
+}
+
+# OUTPUT IP
+output "instance_ip" {
+  value = aws_instance.test.public_ip
+}
+
+output "ssh_command" {
+  value = "ssh -i n8n.pem ubuntu@${aws_instance.test.public_ip}"
+}
