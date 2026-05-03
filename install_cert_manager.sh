930f8fcc9567703bb583c8196748892e0a7bebb3
orvencasido <orvencasidop@gmail.com>
2026-05-03 20:12:23 +0800

    add commands in certs

--- install_cert_manager.sh
+++ install_cert_manager.sh
@@ -3,3 +3,9 @@
 set -e 
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
+
+#to view cert status
+#kubectl get clusterissuer
+
+#to check logs
+#kubectl describe clusterissuer letsencrypt-prod
