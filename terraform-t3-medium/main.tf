17a28bdd70962fdbcef6f2106a6696659083508b
orvencasido <orvencasidop@gmail.com>
2026-05-03 16:40:52 +0800

    config add 20gb block storage

--- terraform-t3-medium/main.tf
+++ terraform-t3-medium/main.tf
@@ -46,6 +46,12 @@ resource "aws_instance" "test" {
     }
   }
 
+  root_block_device {
+    volume_size           = 20
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
   tags = {
     Name = "terraform-spot-t3-medium"
   }
